ea509209f07b98ea3759c6a47f0092758cf9f429
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-02 16:01:30 +0500

    create splash screen

---
+++ lib/ui/widgets/buttons/common_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonButton extends StatelessWidget {
+  const CommonButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
