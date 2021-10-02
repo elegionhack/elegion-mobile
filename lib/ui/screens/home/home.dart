4868300c1dc0c31b8e370bc327799433dc34a546
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-02 22:58:30 +0500

    Add navigation pannel

--- lib/ui/screens/home/home.dart
+++ lib/ui/screens/home/home.dart
@@ -1,15 +1,24 @@
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:elegion/bloc/events/events_bloc.dart';
 import 'package:elegion/ui/widgets/debug_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _slectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       body: BlocBuilder<EventsBloc, EventsState>(
         builder: (context, state) {
@@ -19,6 +28,55 @@ class HomeScreen extends StatelessWidget {
           return const Center(child: CircularProgressIndicator());
         },
       ),
+      bottomNavigationBar: CustomNavigationBar(
+        iconSize: 30.0,
+        scaleFactor: 0.1,
+        selectedColor: theme.primaryColor,
+        strokeColor: theme.primaryColor,
+        unSelectedColor: Colors.grey,
+        backgroundColor: Colors.white,
+        items: [
+          CustomNavigationBarItem(
+            icon: const Icon(Icons.architecture_rounded),
+            title: Text(
+              'Проекты',
+              style: _style(0, theme),
+            ),
+          ),
+          CustomNavigationBarItem(
+            icon: const Icon(Icons.people_alt),
+            title: Text(
+              'Работники',
+              style: _style(1, theme),
+            ),
+          ),
+          CustomNavigationBarItem(
+            icon: const Icon(Icons.home_filled),
+            title: Text(
+              'Главная',
+              style: _style(2, theme),
+            ),
+          ),
+          CustomNavigationBarItem(
+            icon: const Icon(Icons.account_circle),
+            title: Text(
+              'Профиль',
+              style: _style(3, theme),
+            ),
+          ),
+        ],
+        currentIndex: _slectedIndex,
+        onTap: (i) {
+          setState(() => _slectedIndex = i);
+        },
+      ),
+    );
+  }
+
+  TextStyle _style(int index, ThemeData theme) {
+    return TextStyle(
+      color: index == _slectedIndex ? theme.primaryColor : theme.hintColor,
+      fontSize: 12,
     );
   }
 }
