f6ea50afd0f1ca1c5ad82b437d65cb8ff2c9637d
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-02 14:41:37 +0500

    Add bloc provider

--- lib/elegion.dart
+++ lib/elegion.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:elegion/bloc/auth/auth_bloc.dart';
 import 'package:elegion/ui/router/router.gr.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ElegionApp extends StatefulWidget {
   ElegionApp({Key? key}) : super(key: key);
@@ -14,17 +16,24 @@ class ElegionApp extends StatefulWidget {
 class _ElegionAppState extends State<ElegionApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Elegion',
-      routeInformationParser: widget._appRouter.defaultRouteParser(),
-      builder: (_, router) {
-        return router!;
-      },
-      routerDelegate: AutoRouterDelegate(
-        widget._appRouter,
-        navigatorObservers: () => [
-          AutoRouteObserver(),
-        ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(),
+        )
+      ],
+      child: MaterialApp.router(
+        title: 'Elegion',
+        routeInformationParser: widget._appRouter.defaultRouteParser(),
+        builder: (_, router) {
+          return router!;
+        },
+        routerDelegate: AutoRouterDelegate(
+          widget._appRouter,
+          navigatorObservers: () => [
+            AutoRouteObserver(),
+          ],
+        ),
       ),
     );
   }
