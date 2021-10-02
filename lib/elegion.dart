75fa73cca16052cf54a16f55335c167b8ce2414c
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-02 21:29:04 +0500

    Add profiles bloc

--- lib/elegion.dart
+++ lib/elegion.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:elegion/bloc/auth/auth_bloc.dart';
+import 'package:elegion/bloc/profiles/profiles_bloc.dart';
 import 'package:elegion/bloc/projects/projects_bloc.dart';
+import 'package:elegion/repositories/profiles/repository.dart';
 import 'package:elegion/repositories/projects/api_projects_repository/api_projects_repository.dart';
 import 'package:elegion/ui/router/router.gr.dart';
 import 'package:elegion/ui/theme/theme.dart';
@@ -28,6 +30,8 @@ class _ElegionAppState extends State<ElegionApp> {
     G.registerSingleton<HttpClientFactory>(httpFactory);
     G.registerSingleton<ApiProjectsRepository>(
         ApiProjectsRepository(httpFactory));
+    G.registerSingleton<ApiProfilesRepository>(
+        ApiProfilesRepository(httpFactory));
     super.initState();
   }
 
@@ -42,6 +46,11 @@ class _ElegionAppState extends State<ElegionApp> {
           create: (_) => ProjectsBloc(
             G.get<ApiProjectsRepository>(),
           )..add(LoadProjects()),
+        ),
+        BlocProvider<ProfilesBloc>(
+          create: (_) => ProfilesBloc(
+            G.get<ApiProfilesRepository>(),
+          )..add(LoadProfiles()),
         ),
       ],
       child: MaterialApp.router(
