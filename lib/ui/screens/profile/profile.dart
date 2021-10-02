c54560b275e598deff40216169e2600c50f7228f
Tim <76014432+probka00@users.noreply.github.com>
2021-10-03 00:12:49 +0500

    add profile screen

--- lib/ui/screens/profile/profile.dart
+++ lib/ui/screens/profile/profile.dart
@@ -8,85 +8,217 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: 600,
-      child: SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
-            TopPhotoStack(),
-            Container(
-              child: Column(
-                children: [Divider()],
-              ),
-            )
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.symmetric(vertical: 30),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            borderRadius: BorderRadius.circular(300.0),
+                          ),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(300.0),
+                              child: Image.network(
+                                "https://sun9-13.userapi.com/impg/ZBHCI8lzxXifZgZUHFs9HC0Qcq_2r_n0dZ9l5w/TdvIgLwXksA.jpg?size=810x1080&quality=96&sign=f87bea738721b5d94fa8c5555cc12211&type=album",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(25, 170, 0, 10),
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 8),
+                            child: Text(
+                              "437",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              border: Border.all(
+                                width: 3,
+                                color: Colors.white,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(400))),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 2),
+                  child: Text(
+                    "Марина Сергеевна Ильина",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 14),
+                      child: Divider(
+                        color: Theme.of(context).primaryColor,
+                        thickness: 1,
+                        endIndent: 105,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+                          child: Text(
+                            "ИНФОРМАЦИЯ",
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "День рождения:",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "16.08.2012",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[400],
+              indent: 15,
+              endIndent: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "Должность:",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "Жопотер",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[400],
+              indent: 15,
+              endIndent: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "Отпуск:",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "24.24.2424 - 23.23.2323",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[400],
+              indent: 15,
+              endIndent: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Text(
+                    "ПАМЯТКА ЛЕГИОНЕРА:",
+                    style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w700,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    child: IconButton(
+                        icon: Icon(Icons.open_in_new, color: Theme.of(context).primaryColor,), onPressed: () {})),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[400],
+              indent: 15,
+              endIndent: 15,
+            ),
           ],
         ),
       ),
-    ));
-  }
-}
-
-class TopPhotoStack extends StatelessWidget {
-  const TopPhotoStack({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Stack(children: [
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(vertical: 30),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 4,
-                color: Theme.of(context).primaryColor,
-              ),
-              borderRadius: BorderRadius.circular(300.0),
-            ),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(300.0),
-                child: Image.network(
-                  "https://sun9-13.userapi.com/impg/ZBHCI8lzxXifZgZUHFs9HC0Qcq_2r_n0dZ9l5w/TdvIgLwXksA.jpg?size=810x1080&quality=96&sign=f87bea738721b5d94fa8c5555cc12211&type=album",
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
-                )),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(25, 170, 0, 10),
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 8),
-              child: Text(
-                "437",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white),
-              ),
-            ),
-            decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                border: Border.all(
-                  width: 3,
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(400))),
-          ),
-          Container(
-            child: Text(
-              "Марина Сергеевна Ильина",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-        ])
-      ],
     );
   }
 }
