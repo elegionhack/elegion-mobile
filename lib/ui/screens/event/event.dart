bad77d562803115d807bece51dd6a6c1159a0060
Tim <76014432+probka00@users.noreply.github.com>
2021-10-03 07:28:29 +0500

    add event screen

--- lib/ui/screens/event/event.dart
+++ lib/ui/screens/event/event.dart
@@ -5,6 +5,182 @@ class EventScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Event screen')));
+    return Scaffold(
+      body: ListView(
+        children: [
+          TopProjectBar(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Text(
+                  'Описание',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    "Маэстро K.nbr lftn rjywthns b exbn cdjtve  nfkfyne",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  )),
+              Divider(color: Colors.grey[400]),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                  child: Text(
+                    'Участники:',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).primaryColor),
+                  )),
+            ],
+          ),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+          MemberCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class MemberCard extends StatelessWidget {
+  const MemberCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30.0),
+                            child: Image.network(
+                              "https://nomadicibp.com/wp-content/uploads/2019/11/155737_1920.jpg",
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            )),
+                      )
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Text(
+                          "Сергей Сергеевич Сергеев",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          "Мидл разработчик шарпа",
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopProjectBar extends StatelessWidget {
+  const TopProjectBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 240,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://pbs.twimg.com/media/DqabDlYWwAEOnBb.jpg:large"),
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ),
+        Container(
+          height: 240,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      "Игра на лютне",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
