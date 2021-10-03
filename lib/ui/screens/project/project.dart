ca531c4551c75798c79ae46f08a551bfa1b5fe3b
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-03 07:48:24 +0500

    Make small refactor

--- lib/ui/screens/project/project.dart
+++ lib/ui/screens/project/project.dart
@@ -1,5 +1,6 @@
-import 'package:elegion/models/profile/profile.dart';
 import 'package:elegion/models/project/project.dart';
+import 'package:elegion/ui/screens/project/widgets/widgets.dart';
+import 'package:elegion/ui/widgets/profiles/profiles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -82,7 +83,7 @@ class ProjectScreen extends StatelessWidget {
                   height: 170,
                   child: ListView(
                     children: project.workersModels
-                        .map((e) => MemberCard(profile: e!))
+                        .map((e) => ProfileCard(profile: e!))
                         .toList(),
                   ),
                 ),
@@ -92,27 +93,30 @@ class ProjectScreen extends StatelessWidget {
                   endIndent: 15,
                 ),
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                          child: Text(
+                      Text(
                         'Чат в Telegram:',
                         style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w300,
-                            color: Theme.of(context).primaryColor),
-                      )),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
                       GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                              margin: EdgeInsets.only(right: 15),
-                              child: SvgPicture.asset(
-                                'assets/icons/tlg.svg',
-                                height: 25,
-                                width: 25,
-                              ))),
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 15),
+                          child: SvgPicture.asset(
+                            'assets/icons/tlg.svg',
+                            height: 25,
+                            width: 25,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -126,23 +130,24 @@ class ProjectScreen extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                          child: Text(
+                      Text(
                         'Таск трекер:',
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w300,
                             color: Theme.of(context).primaryColor),
-                      )),
+                      ),
                       GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                              margin: EdgeInsets.only(right: 15),
-                              child: SvgPicture.asset(
-                                'assets/icons/task.svg',
-                                height: 25,
-                                width: 25,
-                              ))),
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 15),
+                          child: SvgPicture.asset(
+                            'assets/icons/task.svg',
+                            height: 25,
+                            width: 25,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -152,184 +157,5 @@ class ProjectScreen extends StatelessWidget {
         ],
       ),
     ));
-  }
-}
-
-class MemberCard extends StatelessWidget {
-  const MemberCard({
-    Key? key,
-    required this.profile,
-  }) : super(key: key);
-
-  final Profile profile;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(30.0),
-                          child: profile.avatarUrl != null
-                              ? Image.network(
-                                  profile.avatarUrl!,
-                                  height: 50,
-                                  width: 50,
-                                  fit: BoxFit.cover,
-                                )
-                              : const SizedBox(),
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        child: Text(
-                          profile.fullName,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Text(
-                          profile.position,
-                          style: const TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                                margin: EdgeInsets.only(right: 10),
-                                child: SvgPicture.asset(
-                                  'assets/icons/tlg.svg',
-                                  height: 25,
-                                  width: 25,
-                                ))),
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            borderRadius: const BorderRadius.all(
-              Radius.circular(10),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TopProjectBar extends StatelessWidget {
-  const TopProjectBar({
-    Key? key,
-    required this.project,
-  }) : super(key: key);
-
-  final Project project;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 240,
-          decoration: project.photo != null
-              ? BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      project.photo!,
-                    ),
-                    fit: BoxFit.fitWidth,
-                  ),
-                )
-              : null,
-        ),
-        Container(
-          height: 240,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.transparent, Colors.black.withOpacity(0.2)],
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 240,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: Text(
-                      project.title,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
-                      softWrap: true,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: GestureDetector(
-                        onTap: () {},
-                        child: SvgPicture.asset(
-                          'assets/icons/cal.svg',
-                          height: 25,
-                          width: 25,
-                        )),
-                  )
-                ],
-              ),
-            ],
-          ),
-        )
-      ],
-    );
   }
 }
